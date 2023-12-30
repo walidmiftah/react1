7603651ad5bb3a3b093ec4bfb40e870b3c87ee14
walidmiftah <walidmiftah25@gmail.com>
2023-12-30 12:16:07 +0100

    hckbk

---
+++ src/helloWorld/HelloWorld.js
@@ -0,0 +1,5 @@
+export default function HelloWorld(){
+    return (
+        <div>Hello World</div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import HelloWorld from './helloWorld/HelloWorld';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>
 );
 
